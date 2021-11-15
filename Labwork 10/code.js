d231efd908e01ecc0a7be1f517451ddc1a020df6
cruzmaria444 <91211579+cruzmaria444@users.noreply.github.com>
2021-11-14 18:39:42 -0800

    update

--- Labwork 10/code.js
+++ Labwork 10/code.js
@@ -1,12 +1,13 @@
 
-add = function () {
-  x = jQuery('#op1').html();
-  y = jQuery('#op2').html();
-  alert (x+y)
+add = function() {
+    x = parseInt(jQuery('#op1').html()); // html() is a getter
+    y = parseInt(jQuery('#op2').html());
+    // alert(x + y) //now, adition not concatenation anymore
+    z = x + y // processing
+    jQuery('#result').html(z); // html(inout) is a setter
 }
-
-setup  function () {
-  jQuery('#trigger_the_add').click(add)
+setup = function() {
+    jQuery('#trigger_the_add').click(add)
 }
 
 jQuery(document).ready(setup)
