edf55b0814556d62cc2f96b36620e8b20fa8d4cf
cruzmaria444 <91211579+cruzmaria444@users.noreply.github.com>
2021-11-14 18:35:33 -0800

    update

--- Labwork 10/code.js
+++ Labwork 10/code.js
@@ -2,4 +2,11 @@
 add = function () {
   x = jQuery('#op1').html();
   y = jQuery('#op2').html();
+  alert (x+y)
 }
+
+setup  function () {
+  jQuery('#trigger_the_add').click(add)
+}
+
+jQuery(document).ready(setup)
