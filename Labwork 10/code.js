d43ee009cf0c74c25a5926fbf2115463e9c3e281
cruzmaria444 <91211579+cruzmaria444@users.noreply.github.com>
2021-11-15 10:32:16 -0800

    Update code.js

--- Labwork 10/code.js
+++ Labwork 10/code.js
@@ -7,7 +7,7 @@
     //this func same as appending
     sampl = jQuery('#History').html()
     jQuery('#History').html(sampl + x + ' + ' + y + ' = ' + z + '<br>');
-    console.log (jQuery('#result').val());
+    //console.log (jQuery('#result').val());
 }
 //func for subtraction
 function sub () {
