17e2014de167608e2fc4cf8381e6ff485dcf80dd
cruzmaria444 <91211579+cruzmaria444@users.noreply.github.com>
2021-11-15 08:00:48 -0800

    update

--- Labwork 10/code.js
+++ Labwork 10/code.js
@@ -1,12 +1,38 @@
-
-add = function() {
+  //func for addition
+ function add () {
     x = parseInt(jQuery('#op1').html());
     y = parseInt(jQuery('#op2').html());
     z = x + y
-    jQuery('#result').html(z); 
+    jQuery('#result').html(z);
 }
-setup = function() {
+//func for subtraction
+function sub () {
+   x = parseInt(jQuery('#op1').html());
+   y = parseInt(jQuery('#op2').html());
+   z = x - y
+   jQuery('#result').html(z);
+}
+//func for multiplication
+function mul () {
+   x = parseInt(jQuery('#op1').html());
+   y = parseInt(jQuery('#op2').html());
+   z = x * y
+   jQuery('#result').html(z);
+}
+//func for div
+function div () {
+   x = parseInt(jQuery('#op1').html());
+   y = parseInt(jQuery('#op2').html());
+   z = x / y
+   jQuery('#result').html(z);
+}
+
+ function setup () {
     jQuery('#trigger_the_add').click(add)
+    jQuery('#trigger_the_add').click(sub)
+    jQuery('#trigger_the_add').click(mul)
+    jQuery('#trigger_the_add').click(div)
+
 }
 
 jQuery(document).ready(setup)
