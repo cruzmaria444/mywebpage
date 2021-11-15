a8b998bd769854414ae00e41d12e3772654b7160
cruzmaria444 <91211579+cruzmaria444@users.noreply.github.com>
2021-11-15 09:34:57 -0800

    Update code.js

--- Labwork 10/code.js
+++ Labwork 10/code.js
@@ -42,11 +42,11 @@ function div () {
 
 }
 
-function be_sad() {
+function be_happy() {
     jQuery('img#face').attr('src', 'https://images.unsplash.com/photo-1509909756405-be0199881695?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80')
 }
 
-function be_happy() {
+function be_sad() {
     jQuery('img#face').attr('src', 'https://images.unsplash.com/photo-1597176116047-876a32798fcc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2FkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
 }
 
