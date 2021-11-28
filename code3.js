3346bb063e1ba0fc9b0df3594a3a0fc5d61e6391
cruzmaria444 <91211579+cruzmaria444@users.noreply.github.com>
2021-11-27 22:08:22 -0800

    update

--- code3.js
+++ code3.js
@@ -5,7 +5,7 @@ function process_(data) {
   $('#city_weather').html(data.weather[0].main)
 }
 
-function call_ajax() {
+function get_ajax() {
   $.ajax({
     url: `https://api.openweathermap.org/data/2.5/weather?q=${jQuery('#city_name_input').val()}&appid=8f4372e59d9d1d5c204a8053af0d9d4a&units=metric`,
     type: 'GET',
@@ -14,8 +14,8 @@ function call_ajax() {
 }
 
 function setup() {
-  $('#get_temperature_button').click(call_ajax)
-  $('#get_weather_button').click(call_ajax)
+  $('#get_temperature_button').click(get_ajax)
+  $('#get_weather_button').click(get_ajax)
 }
 
-$(document).ready(setup)
+$(setup)
